802fe89c4f2fb28fb8f8f869058c9a3eb5d4139c
beom-yun <scds0326@naver.com>
2022-07-17 18:22:50 +0900

    weather

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+const images = ['0.jpeg', '1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg'];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement('img');
 
